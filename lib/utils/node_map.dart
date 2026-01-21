64361c1dfeff70d39f4fd5b95338be8b85e676ec
Mohamad Aiman <104369411+aimanzai@users.noreply.github.com>
2026-01-21 15:44:06 +0800

    Add files via upload

---
+++ lib/utils/node_map.dart
@@ -0,0 +1,8 @@
+final Map<String,String> placeToNodeId = {
+  'Cafeteria'    : 'N_Cafeteria',
+  'Toilet Cafe'  : 'N_Toilet_Cafe',
+  'VIP Porch'    : 'N_VIP_Porch',
+  'Musolla'      : 'N_Musolla',
+  'Lobby'        : 'N_Lobby',
+  'Main Entrance': 'N_MainEntrance',
+};
